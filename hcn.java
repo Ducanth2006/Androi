167f4fd66d3b641ec5464b0a71f37103810cd9f5
Ducanth2006 <nicklazada22@gmail.com>
2026-03-14 11:55:36 +0700

    oke

---
+++ hcn.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class hcn{
+    double chieudai;
+    double chieurong;
+    double kqDientich;
+    double kqChuVi;
+    public hcn(double chieudai,double chieurong){
+        this.chieudai=chieudai;
+        this.chieurong=chieurong;
+    }
+
+
+    public double tinhDienTich(){
+        this.kqDientich=chieudai*chieurong;
+        return kqDientich;
+    }
+    public double chuVi(){
+        this.kqChuVi=(chieudai+chieurong)/2;
+        return kqChuVi;
+    }
+    public void hienthi() {
+        System.out.println(tinhDienTich());
+        System.out.println(chuVi());
+
+    }
+
+}
+public class hinhchunhat {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        hcn s1= new hcn(5,6);
+        s1.hienthi();
+
+    }
+}
